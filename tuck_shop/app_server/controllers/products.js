645e796f11a81a0ec4eb0dcf89219ea51fe51ec3
Katlego Enoch <i@katlego.cloud>
2024-06-02 18:43:39 +0200

    moved data from views to controllers

--- tuck_shop/app_server/controllers/products.js
+++ tuck_shop/app_server/controllers/products.js
@@ -1,5 +1,31 @@
 const productsList = (req, res) => {
-    res.render('product-list', {title: 'Products'});
+    res.render('product-list', {
+        title: 'ShopApp - Eat now, Pay later.',
+        pageHeader: {
+            title: 'ShopApp',
+            strapline: 'Find a meal to eat while working!'
+        },
+        products: [{
+            name: 'Noodles',
+            description: 'Two minute noodles',
+            rating: 3,
+            price: 'R5.00',
+            options: ['Durban Curry','Chicken Mayo','Beef']
+        },{
+            name: 'Energy Drink',
+            description: 'Boost your senses',
+            rating: 4,
+            price: 'R20.00',
+            options: ['Monster','Red Bull','MoFaya']
+        },{
+            name: 'Chips',
+            description: 'Snack to keep the hunger away',
+            rating: 2,
+            price: 'R15.00',
+            options: ['Simba','Lays','Niknaks']
+        }],
+        sidebar: 'Looking for a quick meal? ShopApp helps you grab a meal, sign for it and only pay at the end of the month. Eat as much as you want and keep track of your bill.'
+    });
 };
 
 const productInfo = (req, res) => {
