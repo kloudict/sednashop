20ee2aabafd59f818a2a6b9a43a0956510a7075c
Katlego Enoch <i@katlego.cloud>
2024-06-03 17:25:21 +0200

    Mongoose connected to MongoDB

---
+++ tuck_shop/app_server/models/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const dbURI = 'mongodb://localhost/ShopApp';
+mongoose.connect(dbURI);
+
+mongoose.connection.on('connected', () => {
+    console.log(`Mongoose connected to ${dbURI}`);
+});
+
+mongoose.connection.on('error', err => {
+    console.log('Mongoose connection error: ', err);
+});
+
+mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose disconnected');
+});
