e53986e3d4fb0e4b3f3897215a0dce22e3f4237a
Katlego Enoch <i@katlego.cloud>
2024-06-04 11:01:12 +0200

    Worked on GET and POST API requests

--- tuck_shop/app_api/controllers/products.js
+++ tuck_shop/app_api/controllers/products.js
@@ -1,31 +1,84 @@
 const mongoose = require('mongoose');
 const Prod = mongoose.model('Product');
 
-const productsReadAll = (req, res) => {
-    res
-        .status(200)
-        .json({status: 'read all products'});
+const productsReadAll = async (req, res) => {
+    const products = await Prod.find({});
+    if(products.length == 0){
+        res
+            .status(404)
+            .json({"message":"no products found."});
+    }
+    else{
+        res
+            .status(200)
+            .json(products);
+
+    }
 };
-const productsCreateOne = (req, res) => {
-    res 
+const productsCreateOne = async (req, res) => {
+    const product = await Prod.create({
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        options: req.body.options.split(','),
+        rating: parseInt(req.body.rating),
+        days: req.body.days2,
+        opening: req.body.opening2,
+        closing: req.body.closing2,
+        closed: req.body.closed2
+    });
+    if(!product){
+        res
+            .status(400)
+            .json({"message":"could not create product"});
+    }
+    else{
+        res 
         .status(201)
-        .json({status: 'created one product'});
+        .json(product);
+    }
 
 };
-const productsReadOne = (req, res) => {
-    res
-        .status(200)
-        .json({status: 'read one product'});
+const productsReadOne = async (req, res) => {
+    //incorrect length of productId will cause the server to crash
+    if(req.params.productId.length != 24 || !req.params.productId) {
+        res
+            .status(400)
+            .json({"message":"invalid productId"});
+    }
+    else{
+        const product = await Prod.findById(req.params.productId);
+        if(!product){
+            res
+                .status(404)
+                .json({"messsage": "product not found."});
+        }
+        else{
+            res
+            .status(200)
+            .json(product);
+        }
+    }    
 };
 const productsUpdateOne = (req, res) => {
+    if(req.params.product.length == 0 || !req.params.productId){
+        res
+            .status(400)
+            .json({"message":"invalid product Id"});
+    }
+    else{
+
+    }
     res
         .status(201)
         .json({status: 'updated one product'});
 };
 const productsDeleteOne = (req, res) => {
-    res
-        .status(204)
-        .json({status: 'deleted one product'});
+
+        res
+            .status(400)
+            .json({"message":"invalid product Id"});
+   
 };
 
 module.exports = {
--- tuck_shop/app_api/controllers/reviews.js
+++ tuck_shop/app_api/controllers/reviews.js
@@ -1,20 +1,119 @@
 const mongoose = require('mongoose');
 const Prod = mongoose.model('Product');
 
-const reviewsReadOne = (req, res) => {
-    res
-        .status(200)
-        .json({status: 'read one review'});
+const reviewsReadOne = async (req, res) => {
+    if(req.params.productId.length == 0 || !req.params.productId){
+        return res
+            .status(400)
+            .json({"message":"invalid product Id"});
+    }
+    else{
+        product = await Prod.findById(req.params.productId);
+        if(!product){
+            return res
+                .status(404)
+                .json({"message":"product not found"});
+        }
+        else{
+            if(product.reviews && product.reviews.length > 0){
+                review = product.reviews.id(req.params.reviewId);
+                if(!review){
+                    return res
+                                .status(400)
+                                .json({"message":"review not found."});
+                }
+                else{
+                    response = {
+                        product: {
+                            product: product.name,
+                            id: req.params.productId
+                        },
+                        review
+                    };
+                    return res
+                        .status(200)
+                        .json(response);
+                }
+            }
+            else{
+                return res
+                    .status(404)
+                    .json({"message":"reviews not found."});
+            }
+        }
+        
+    }
 };
-const reviewsCreateOne = (req, res) => {
-    res
-    .status(201)
-    .json({status: 'created one review'});
+const reviewsCreateOne = async (req, res) => {
+    if(req.params.productId.length == 0 || !req.params.productId){
+        return res
+            .status(400)
+            .json({"message":"invalid product Id"});
+    }
+    else{
+        product = await Prod.findById(req.params.productId);
+        if(!product){
+            return res
+                .status(404)
+                .json({"message":"product not found"});
+        }
+        else{
+            const review = {
+                author: req.body.author,
+                rating: parseInt(req.body.rating),
+                reviewText: req.body.reviewText
+            };
+            product.reviews.push(review);
+            product.save().then(() => {
+                res
+                    .status(201)
+                    .json(review);
+            }).catch(err => {
+                console.log(err);
+            })
+        }    
+    }
 };
-const reviewsUpdateOne = (req, res) => {
-    res
-    .status(201)
-    .json({status: 'updated one review'});
+const reviewsUpdateOne = async (req, res) => {
+      if(req.params.productId.length != 24 || !req.params.productId) {
+        res
+            .status(400)
+            .json({"message":"invalid productId"});
+    }
+    else{
+        const product = await Prod.findById(req.params.productId);
+        if(!product){
+            res
+                .status(404)
+                .json({"messsage": "product not found."});
+        }
+        else{
+            res
+            .status(200)
+            .json(product);
+        }
+    }
+    const product = await Prod.create({
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        options: req.body.options.split(','),
+        rating: parseInt(req.body.rating),
+        days: req.body.days2,
+        opening: req.body.opening2,
+        closing: req.body.closing2,
+        closed: req.body.closed2
+    });
+    if(!product){
+        res
+            .status(400)
+            .json({"message":"could not create product"});
+    }
+    else{
+        res 
+        .status(201)
+        .json(product);
+    }
 };
 const reviewsDeleteOne = (req, res) => {
     res
